33e139b6ef7905c6fee36a303ed10adae5bcc3e8
Joy <love08220724@gmail.com>
2019-09-08 22:54:27 +0800

    [enhance] add a dynamic chart

--- server/home/views.py
+++ server/home/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import plotly.express as px
+from plotly.offline import plot
+import plotly.graph_objs as go
+# from django.http import HttpResponse
 
 
 def index(request):
-	#return HttpResponse('<table><tr>222<td>123</td></tr></table>')
-	data = {
-		'tt': '<table>123</table>'
-	}
-	return render(request, 'home/abc.html', data)
+    # return HttpResponse('<table><tr>222<td>123</td></tr></table>')
+    gapminder = px.data.gapminder().query("country=='Canada'")
+
+    fig = px.line(gapminder, x="year", y="lifeExp", title='Life exp ectancy in Canada')
+    plot_div = plot(fig, output_type='div', include_plotlyjs=False)
+
+    data = {
+        'tt': '123',
+        'joy': plot_div,
+    }
+    return render(request, 'home/abc.html', data)
 
 # Create your views here.
